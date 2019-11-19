6c03909fe7abdc5a2890a6744658617b28c479f6
xuls <839046422@qq.com>
2019-11-19 16:40:30 +0800

    测试历史数据

---
+++ src/swing2019_11_19/testGit.java
@@ -0,0 +1,12 @@
+package swing2019_11_19;
+
+/**
+ * @author auas
+ * 用来测试git冲突的java文件
+ */
+public class testGit {
+	public static void main(String[] args) {
+		System.out.println("我是测试git的java文件");
+		System.out.println("我是用来测试历史数据的");
+	}
+}
