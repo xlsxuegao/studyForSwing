065c0b09238516decf387d0da497c28695237111
xuls <839046422@qq.com>
2019-11-19 16:41:37 +0800

    测试历史数据2

--- src/swing2019_11_19/testGit.java
+++ src/swing2019_11_19/testGit.java
@@ -8,5 +8,6 @@ public class testGit {
 	public static void main(String[] args) {
 		System.out.println("我是测试git的java文件");
 		System.out.println("我是用来测试历史数据的");
+		System.out.println("我也是用来测试历史数据的");
 	}
 }
